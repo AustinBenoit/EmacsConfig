bffd9a139af2199142a38c9194e42a44b9eeddaf
Austin Benoit <22805659+AustinBenoit@users.noreply.github.com>
2021-10-15 13:00:04 -0700

    Now used more use-package

--- init.el
+++ init.el
@@ -1,19 +1,20 @@
 ;;================================================================
+
 ;; Basic Set ups
+
 ;; ===============================================================
 (tool-bar-mode -1)
 (setq visible-bell t)
 (setq inhibit-startup-message t)
-
 (set-face-attribute 'default nil :font "DejaVu Sans Mono" :height 120)
 
 ;; (setq default-directory "c:/")   Home is fine on linux
 (global-linum-mode t)               ;; Enable line numbers globally
 
-
 ;; ===============================================================
 ;; Packages
 ;; ===============================================================
+
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -22,9 +23,10 @@
 
 (package-initialize)
 (unless package-archive-contents
- (package-refresh-contents))
+  (package-refresh-contents))
 
 ;; Initialize use-package on non-Linux platforms
+
 (unless (package-installed-p 'use-package)
    (package-install 'use-package))
 
@@ -33,13 +35,13 @@
 
 ;; General set ups ==============================================
 
-
 ;; Remove the echo from CMD
 ;;(defun my-comint-init ()
 ;;  (setq comint-process-echoes t))
 ;;(add-hook 'comint-mode-hook 'my-comint-init)
 
 ;; spell checking for text and programing
+
 (add-hook 'text-mode-hook 'flyspell-mode)
 (add-hook 'prog-mode-hook 'flyspell-prog-mode)
 
@@ -55,8 +57,11 @@
 ;; ===============================================================
 ;; ORG MODE SET UPS
 ;; ===============================================================
+
 (require 'org)
+
 (define-key global-map "\C-cl" 'org-store-link)
+
 (define-key global-map "\C-ca" 'org-agenda)
 
 (setq org-todo-keywords
@@ -67,23 +72,27 @@
 ;; ===============================================================
 ;; Set up Hunspell for spell checking
 ;; ===============================================================
+
 (add-to-list 'exec-path "C:/hunspell/hunspell-1.3.2-3-w32-bin/bin")
 
 (setq ispell-program-name (locate-file "hunspell"
-      exec-path exec-suffixes 'file-executable-p))
+    exec-path exec-suffixes 'file-executable-p))
 
 (setq ispell-local-dictionary "en_US")
 
 (setq ispell-local-dictionary-alist
       '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "en_US") nil utf-8)))
 
+
 ;; ===============================================================
 ;; Set Up Programming environment
 ;; ===============================================================
 
 ;; General =======================================================
-(use-package flycheck)
-(global-flycheck-mode)
+
+(use-package flycheck
+  :init (global-flycheck-mode 1)
+  (setq flycheck-python-flake8-executable '("C:/Users/sesa638306/AppData/Local/Programs/Python/Python37/Scripts/flake8.exe")))
 
 (use-package swiper)
 
@@ -91,6 +100,7 @@
   :hook (prog-mode . rainbow-delimiters-mode))
 
 ;; Ivy ===========================================================
+
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper)
@@ -117,33 +127,31 @@
          ("C-r" . 'counsel-minibuffer-history)))
 
 (use-package ivy-rich
+  :diminish
   :init
   (ivy-rich-mode 1))
 
 ;; White Space ===================================================
-(require 'whitespace)
 
-(setq whitespace-style (quote (face spaces tabs space-mark tab-mark)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(whitespace-space ((t (:background nil :foreground "grey30")))))
-
-(add-hook 'prog-mode-hook 'whitespace-mode)
+(use-package whitespace
+  :init
+  (setq whitespace-style (quote (face spaces tabs space-mark tab-mark)))
+  (custom-set-faces
+   '(whitespace-space ((t (:background nil :foreground "grey30")))))
+  :hook (prog-mode . whitespace-mode))
 
 ;; Projectile ====================================================
+
 (use-package projectile
   :diminish projectile-mode
   :config (projectile-mode)
   :custom ((projectile-completion-system 'ivy))
   :bind-keymap
-  ("C-c p" . projectile-command-map)
+ ("C-c p" . projectile-command-map)
   :init
   ;; NOTE: Set this to the folder where you keep your Git repos!
   (when (file-directory-p "~/Projects")
-    (setq projectile-project-search-path '("~/Projects")))
+    (setq projectile-project-search-path '("~/Projects", "C:\\Dev\\Git\\HEP\\HEP")))
   (setq projectile-switch-project-action #'projectile-dired))
 
 (use-package counsel-projectile
@@ -154,18 +162,20 @@
 (use-package magit)
 
 ;; Python ========================================================
-(use-package elpy)
-(elpy-enable)
-(when (require 'flycheck nil t)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+(use-package elpy
+  :ensure t
+  :init
+  (elpy-enable)
+  :config '(elpy-test-pytest-runner-command '("py.test" "--variant" "VARIANT_ION7400"))
+  )
 
 (put 'dired-find-alternate-file 'disabled nil)
+
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (magit which-key use-package rainbow-delimiters magit-section ivy-rich go-mode git-commit flycheck elpy doom-themes counsel-projectile))))
+;; custom-set-variables was added by Custom.
+;; If you edit it by hand, you could mess it up, so be careful.
+;; Your init file should contain only one such instance.
+;; If there is more than one, they won't work right.
+'(package-selected-packages
+   '(magit which-key use-package rainbow-delimiters magit-section ivy-rich go-mode git-commit flycheck elpy doom-themes counsel-projectile)))
